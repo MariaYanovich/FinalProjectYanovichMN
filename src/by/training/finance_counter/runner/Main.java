efc8e34880ca31f150c812db24fe2f3a2d4ecb8b
MariaYanovich <maryanovichs@gmail.com>
2020-01-29 05:00:49 +0300

    "convert project to maven"

--- src/by/training/finance_counter/runner/Main.java
+++
@@ -1,9 +0,0 @@
-package by.training.finance_counter.runner;
-
-import static by.training.finance_counter.viewer.Menu.menu;
-
-public class Main {
-    public static void main(String[] args) {
-        menu();
-    }
-}
